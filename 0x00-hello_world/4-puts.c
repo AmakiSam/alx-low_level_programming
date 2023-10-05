dfd76825fe4231f0fce5e2cfa34c0c2a6a9d91c2
AmakiSam <samer.works93@gmail.com>
2023-10-05 16:08:50 -0700

    printf done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- * main - main entry point
+ * Main - main entry point
  *
  * Description: 'Your program should end with the value 0'
  *
- * return: always 0 (success)
+ * Return: always 0 (success)
  */
 
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
