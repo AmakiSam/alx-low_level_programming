16abce4055d5f6ee7f6280789dd5043137cd9432
AmakiSam <samer.works93@gmail.com>
2023-10-09 17:27:57 -0700

    CombAgain

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: All possible different combinations of two digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int z;
+	int w;
+
+	for (z = '0'; z <= '9'; z++)
+	{
+		for (w = '0'; w <= '9'; w++)
+		{
+		if (z < w)
+		{
+			putchar(z);
+			putchar(w);
+			if (z != '8' || (z == '8' && w != '9'))
+			putchar(',');
+			putchar(' ');
+		}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
