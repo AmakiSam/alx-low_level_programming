18efe855f4fa709381b80d099d1e2cd4ccbcf045
AmakiSam <samer.works93@gmail.com>
2023-10-12 14:57:48 -0700

    AlphaLowerx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabets x10, lowercase
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	char y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 'a'; y <= 'z'; y++)
+			_putchar(y);
+	}
+	_putchar('\n');
+
+}
