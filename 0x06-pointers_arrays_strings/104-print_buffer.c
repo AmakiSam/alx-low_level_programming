945dd4aa1ffcb4f67d50b500d1c3f10ae3956ed7
AmakiSam <samer.works93@gmail.com>
2023-11-05 04:15:11 -0800

    buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,28 +10,26 @@
 
 void print_buffer(char *b, int size)
 {
-	int i, j;
+	int i;
+	int j;
 
 	if (size <= 0)
 	{
 		printf("\n");
 		return;
 	}
-
 	for (i = 0; i < size; i += 10)
 	{
 		printf("%08x: ", i);
 
-		for (j = i; j < i + 10; j++)
+		for (j = i; j < i + 10; j += 2)
 		{
 			if (j < size)
-				printf("%02x", b[j]);
+				printf("%02x%02x ", b[j], b[j + 1]);
 			else
 				printf("  ");
-
-			if (j % 2)
-				printf(" ");
 		}
+
 		printf(" ");
 
 		for (j = i; j < i + 10; j++)
