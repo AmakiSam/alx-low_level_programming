079965f74ce3cf1488273361a1e9504c1ccff5f6
AmakiSam <samer.works93@gmail.com>
2023-12-13 05:29:29 -0800

    fibo3

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - print Fibonacci number to 98
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long int fib1 = 1;
+	unsigned long int fib2 = 2;
+	unsigned long int fib3;
+
+	printf("%lu, %lu, ", fib1, fib2);
+	for (int i = 3; i <= 98; i++)
+	{
+		fib3 = fib1 + fib2;
+		printf("%lu", fib3);
+		if (i != 98)
+		{
+			printf(", ");
+		}
+		fib1 = fib2;
+		fib2 = fib3;
+	}
+	printf("\n");
+	return (0);
+}
