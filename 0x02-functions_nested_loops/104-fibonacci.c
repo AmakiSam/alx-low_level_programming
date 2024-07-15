77a16bea3ef6bd8151e234d96a7a6eced676f463
AmakiSam <samer.works93@gmail.com>
2024-07-15 05:58:31 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,44 +1,70 @@
 #include "main.h"
 
 /**
- * main - entry point, prints first 98 fibonacci numbers
- * Return: always 0
+ * main - Entry point, calls the fibonacci function
+ * Return: Always 0
  */
 int main(void)
 {
-	unsigned long int a = 1, b = 2, c;
-	int i;
-
-	_putchar('1');
-	_putchar(',');
-	_putchar(' ');
-	_putchar('2');
-
-	for (i = 3; i <= 98; i++)
-	{
-		c = a + b;
-		_putchar(',');
-		_putchar(' ');
-		print_num(c);
-		a = b;
-		b = a;
-	}
-	_putchar('\n');
-	return (0);
+    fibonacci();
+    return (0);
 }
+
 /**
- * print_num - prints an unsigned long int number
- * @n: number to print
+ * fibonacci - Prints the first 98 Fibonacci numbers
+ * Description: Starting with 1 and 2, separated by comma and space
+ */
+void fibonacci(void)
+{
+    unsigned long int a = 1, b = 2, c;
+    int i;
+
+    print_num(a);
+    print_num(b);
+
+    for (i = 3; i <= 98; ++i)
+    {
+        c = a + b;
+        print_num(c);
+        a = b;
+        b = c;
+
+        if (i < 98)
+        {
+            _putchar(',');
+            _putchar(' ');
+        }
+    }
+
+    _putchar('\n');
+}
+
+/**
+ * print_number - Prints an unsigned long int number
+ * @n: Number to print
+ *
+ * Description: This function prints a number digit by digit
  */
 void print_num(unsigned long int n)
 {
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
+    unsigned long int divisor = 1;
+    int digit;
+    
+    if (n == 0)
+    {
+        _putchar('0');
+        return;
+    }
 
-	if (n / 10)
-		print_num(n / 10);
-	_putchar(n % 10 + '0');
+    while (n / divisor >= 10)
+    {
+        divisor *= 10;
+    }
+
+    while (divisor != 0)
+    {
+        digit = (n / divisor) % 10;
+        _putchar(digit + '0');
+        divisor /= 10;
+    }
 }
