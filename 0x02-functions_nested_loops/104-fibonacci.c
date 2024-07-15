053434d72bc0b63b769301620a4f039d9f0537bb
AmakiSam <samer.works93@gmail.com>
2024-07-15 07:55:39 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,44 +1,30 @@
 #include <stdio.h>
 
-#define MAX_DIGITS 1000
-
 /**
 * main - Entry point
 * Return: Always 0
 */
 int main(void)
 {
-int fib1[MAX_DIGITS] = {0};
-int fib2[MAX_DIGITS] = {0};
-int fib3[MAX_DIGITS] = {0};
-int temp[MAX_DIGITS] = {0};
+unsigned int fib1 = 1;
+unsigned int fib2 = 2;
+unsigned int fib3;
 int i;
 
-fib1[0] = 1;
-fib2[0] = 2;
 
-
-printf("%d, %d", fib1[0], fib2[0]);
+printf("%u, %u", fib1, fib2);
 
 
 for (i = 3; i <= 98; i++)
 {
+fib3 = fib1 + fib2;
 
-int carry = 0;
-for (int j = 0; j < MAX_DIGITS; j++)
-{
-int sum = fib1[j] + fib2[j] + carry;
-fib3[j] = sum % 10;
-carry = sum / 10;
-}
 
-printf(", %d", fib3[MAX_DIGITS - 1]);
+printf(", %u", fib3);
 
-for (int j = 0; j < MAX_DIGITS; j++)
-{
-fib1[j] = fib2[j];
-fib2[j] = fib3[j];
-}
+
+fib1 = fib2;
+fib2 = fib3;
 }
 
 printf("\n");
