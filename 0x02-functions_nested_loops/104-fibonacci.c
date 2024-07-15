d4cc7b995991b95b1f2c6e978379768acfb82119
AmakiSam <samer.works93@gmail.com>
2024-07-15 05:50:52 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,25 +1,44 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-* main - prints the first 98 Fibonacci numbers, starting with 1 and 2
-* Return: 0
-*/
+ * main - entry point, prints first 98 fibonacci numbers
+ * Return: always 0
+ */
 int main(void)
 {
 	unsigned long int a = 1, b = 2, c;
 	int i;
 
-	printf("1, 2");
+	_putchar('1');
+	_putchar(',');
+	_putchar(' ');
+	_putchar('2');
 
-	for (i = 3; i <= 98; ++i)
+	for (i = 3; i <= 98; i++)
 	{
 		c = a + b;
-		printf(", %lu", c);
+		_putchar(',');
+		_putchar(' ');
+		print_num(c);
 		a = b;
-		b = c;
+		b = a;
+	}
+	_putchar('\n');
+	return (0);
+}
+/**
+ * print_num - prints an unsigned long int number
+ * @n: number to print
+ */
+void print_num(unsigned long int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
 	}
 
-	printf("\n");
-
-	return (0);
+	if (n / 10)
+		print_num(n / 10);
+	_putchar(n % 10 + '0');
 }
