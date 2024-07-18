8e12239b23f6628b46e26f76dbe6ca3a381c1d6f
AmakiSam <samer.works93@gmail.com>
2024-07-18 03:18:47 -0700

    100-realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,18 +12,17 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
 	unsigned int i;
+	char *old_ptr;
 
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
 	if (ptr == NULL)
 	{
 		return (malloc(new_size));
 	}
-
 	if (new_size == old_size)
 	{
 		return (ptr);
@@ -35,9 +34,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
+	old_ptr = (char *)ptr;
 	for (i = 0; i < old_size && i < new_size; i++)
 	{
-		*((char *)new_ptr + 1) = *((char *)ptr + i);
+		((char *)new_ptr)[i] = old_ptr[i];
 	}
 	free(ptr);
 	return (new_ptr);
